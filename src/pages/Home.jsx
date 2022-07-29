6e71d82c2048b9d4d8e5b091ce4bb0a659aceec0
Ammar <ammarelw@gmail.com>
2022-07-29 17:11:51 +0100

    setting the environment and the popular food component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Popular from "../components/Popular";
+import Veggies from "../components/Veggies";
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Popular />
+      <Veggies />
+    </div>
+  )
+}
+
+export default Home;
