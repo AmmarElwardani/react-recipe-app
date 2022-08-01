fde56ccac2ef146368a1a536aa6351929694881b
Ammar <ammarelw@gmail.com>
2022-08-01 14:41:39 +0100

    setting the recipe details component

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
+import { Link } from "react-router-dom";
 const Popular = () => {
   const [popular, setPopular] = useState([]);
 
@@ -18,10 +19,9 @@ const Popular = () => {
         `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
       );
       const data = await api.json();
-        localStorage.setItem("popular", JSON.stringify(data.recipes))
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
       setPopular(data.recipes);
       console.log(data.recipes);
-     
     }
   };
 
@@ -42,9 +42,11 @@ const Popular = () => {
             return (
               <SplideSlide key={recipe.id}>
                 <Card>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
-                  <Gradient />
+                  <Link to={"/recipe/" + recipe.id}>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <Gradient />
+                  </Link>
                 </Card>
               </SplideSlide>
             );
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,12 +3,14 @@ import Home from "./Home";
 import Cuisine from "./Cuisine";
 import { Routes, Route } from "react-router-dom";
 import Searched from "./Searched";
+import Recipe from "./Recipe";
 const Pages = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/cuisine/:type" element={<Cuisine />} />
       <Route path="/searched/:search" element={<Searched />} />
+      <Route path="/recipe/:id" element={<Recipe />} />
     </Routes>
   );
 };
